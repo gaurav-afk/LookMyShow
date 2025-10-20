d9d47f848b02841b3512969b939216101e57b97d
gaurav-afk <gauravcreator123@gmail.com>
2025-10-20 12:17:06 +0530

    fix: BookingSuccessScreen ui text alignment and color change

--- app/src/main/java/com/towerofapp/lookmyshow/ui/view/home/BookingSuccessScreen.kt
+++ app/src/main/java/com/towerofapp/lookmyshow/ui/view/home/BookingSuccessScreen.kt
@@ -6,12 +6,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -19,9 +23,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.google.android.play.integrity.internal.s
 import com.towerofapp.lookmyshow.ui.components.model.Seat
+import org.maplibre.android.style.layers.PropertyFactory.backgroundColor
 import kotlin.collections.joinToString
 
 @Composable
@@ -36,52 +43,65 @@ fun BookingSuccessScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFE8F5E9))
-            .padding(horizontal = 30.dp),
-
+            .padding(horizontal = 60.dp),
         contentAlignment = Alignment.Center
     ) {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
+//            modifier = Modifier
+//                .background(Color.Red),
+            horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Icon(
-                imageVector = Icons.Default.CheckCircle,
-                contentDescription = "Success",
-                tint = Color(0xFF4CAF50),
-                modifier = Modifier.size(100.dp)
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    imageVector = Icons.Default.CheckCircle,
+                    contentDescription = "Success",
+                    tint = Color(0xFF4CAF50),
+                    modifier = Modifier.size(100.dp),
+                )
+                Text(
+                    text = "Booking Successful!",
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = Color(0xFF57b55f),
+                    fontWeight = FontWeight.Medium
+                )
+            }
 
-            Text(
-                text = "Booking Successful!",
-                style = MaterialTheme.typography.headlineMedium,
-                color = Color.Black
-            )
             Text(
                 text = "Movie: ${movieTitle}",
                 style = MaterialTheme.typography.bodyMedium,
                 color = Color.DarkGray
             )
             Text(
-                text = "Seats booked: ${bookedSeats.joinToString { "${it}" }}",
-                style = MaterialTheme.typography.bodyMedium,
-                color = Color.DarkGray
-            )
-            Text(
                 text = "Theater: ${theater}",
                 style = MaterialTheme.typography.bodyMedium,
-                color = Color.DarkGray
+                color = Color.Gray
+            )
+            Text(
+                text = "Seats booked: ${bookedSeats.joinToString { "${it}" }}",
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Gray
             )
             Text(
                 text = "Paid: ₹${price}",
                 style = MaterialTheme.typography.bodyMedium,
-                color = Color.DarkGray
+                color = Color.Gray
             )
-
-            Button(
-                onClick = {navController.navigate("home")},
-                modifier = Modifier.padding(top = 16.dp)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
             ) {
-                Text(text = "Done")
+                Button(
+                    onClick = { navController.navigate("home") },
+                    modifier = Modifier.padding(top = 16.dp)
+                ) {
+                    Text(text = "Done")
+                }
             }
         }
     }
